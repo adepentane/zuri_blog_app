14d8837c540c360795741d78cfd2744fe0b26e26
adepentane <adepentane5@gmail.com>
2021-04-27 11:21:30 +0100

    Blog works, blog page works, home page works, post.bode in Post model changed to post.body and migrations done and effected.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 from . import views
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home')
+    path('', BlogListView.as_view(), name='home'),
+    path('post/new', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_details')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import CreateView  # To Create New Blog Posts from the FrontEnd
 from .models import Post
 
 
@@ -7,3 +8,14 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_details.html'
+
+
+class BlogCreateView(CreateView):  # new
+    model = Post
+    template_name = 'post_new.html '
+    fields = ['title ', 'author', 'body']
