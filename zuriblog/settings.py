04a2dd041059aaa1eb1c008733e4268a49439c3d
adepentane <adepentane5@gmail.com>
2021-04-27 16:49:07 +0100

    The Python requirement was updated to python-3.7.10  in the runtime.txt file and fixed statis errors

--- zuriblog/settings.py
+++ zuriblog/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = [str(BASE_DIR.joinpath('staticfiles'))]
+STATIC_ROOT = [str(BASE_DIR.joinpath('static'))]
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [str(BASE_DIR.joinpath('static'))]
