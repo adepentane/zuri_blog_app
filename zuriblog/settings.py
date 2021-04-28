40cdeb4ccb104e41ecae9e2a715784a9e5348241
adepentane <adepentane5@gmail.com>
2021-04-28 12:21:14 +0100

    Login and Logout workingas expected

--- zuriblog/settings.py
+++ zuriblog/settings.py
@@ -132,4 +132,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home' # new
+
 django_heroku.settings(locals())
